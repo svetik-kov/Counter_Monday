9ebe4f0094cdb0c772da1600486480956afb11a5
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-07-30 16:42:00 +0300

    выравнивание

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Button";
+import s from './Buttom.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [a, setA] = useState(0)
+
+    const addButton1 = () => {
+        if (a < 5)
+        {setA(++a)}
+
+    }
+    const addButton2 = () => {
+        setA(0)
+    }
+
+    const InputClasses = a === 5 ? s.performance : s.stopPerformance
+    const Button1Classes= a<5 ? s.active:''
+    const Button2Classes= a>0 ?s.active:''
+
+    return (
+        <div className={s.rectangle}>
+            <div><h1 className={InputClasses}>{a}</h1></div>
+            <div className={s.total}>
+                <Button className={Button1Classes} name={'inc'}  onClick={addButton1}/>
+                <Button className={Button2Classes} name={'reset'} onClick={addButton2}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
